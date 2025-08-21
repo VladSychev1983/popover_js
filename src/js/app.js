03bd485ce1c006872703cb6af08af8bc1855249e
Vladislav Sychev <vladsychev1983@gmail.com>
2025-08-21 13:02:24 +0300

    Was repaired unit tests

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,3 @@ import PopOver from "./popover";
 const container = document.querySelector(".container");
 const popoverObj = new PopOver(container);
 popoverObj.insertWidget();
-console.log("app included");
